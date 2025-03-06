dee192b7d554620512cc8989643e5a8208db99b3
mansoor zafar <mansoorahmadzafarwork@gmail.com>
2025-03-06 15:30:59 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: "/portfolio",
   output: "export",  // <=== enables static exports
+  assetPrefix: "/portfolio",
   reactStrictMode: true,
 };
 
